b91b07b98db0d3d785135b9ba7b4545615e6976f
Nova Rain <novaraine@outlook.dk>
2018-10-10 21:58:21 +0200

    Reworked to use discord.net 2 and it's command system

---
+++ DiscordHex/Services/CommonCommands.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiscordHex.Services
+{
+    public class CommonCommands
+    {
+        public EmbedBuilder LoveSomeone(IUser[] users, IReadOnlyCollection<SocketRole> mentionedRoles, string caster)
+        {
+            var e = new EmbedBuilder();
+            
+            if (users == null || !users.Any())
+            {
+                e.Description = $"{caster} seems to love.. everyone! How nice :)";
+                return e;
+            }
+
+            switch (users.First().Id)
+            {
+                // User specific ones
+                case 122444132156309506:
+                    e.ImageUrl = "https://cdn.weeb.sh/images/r17lwymiZ.gif";
+                    e.Description = $"Feel the love damnit!";
+                    break;
+                case 174518951085211648:
+                    e.Description = $"{users.First().Username} Ultimate cuddlepuff! you got loved by {caster}";
+                    e.ImageUrl = "https://cdn.weeb.sh/images/SJYxIUmD-.gif";
+                    break;
+                case 271120668450357258:
+                    e.ImageUrl = "https://cdn.weeb.sh/images/By03IkXsZ.gif";
+                    e.Description = $"{users.First().Username} is the cutest smol bean! Everyone loves you <3";
+                    break;
+
+                // Default = everyone else
+                default:
+                    e.ImageUrl = "https://cdn.weeb.sh/images/HkzArUmvZ.gif";
+                    e.Description = $"{users.First().Username}!! {caster} loves you <3";
+                    break;
+            }
+
+            return e;
+        }
+    }
+}
---
+++ DiscordHex/Services/FfxivSpellService.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiscordHex.Services
+{
+    public class FfxivSpellService
+    {
+        public EmbedBuilder CastEsuna(IUser[] users, IReadOnlyCollection<SocketRole> mentionedRoles, string caster)
+        {
+            var e = new EmbedBuilder();
+
+            if (mentionedRoles.Count > 0)
+            { // no mentioning roles 
+                e.Description = "Don't tag groups! Noone likese that..";
+                return e;
+            }
+
+            var target = string.Empty;
+
+            if (users != null && users.Any())
+            {
+                target = users.First().Username;
+            }
+            
+
+            if (string.IsNullOrEmpty(target))
+            {
+                e.ImageUrl = "https://ffxiv.consolegameswiki.com/mediawiki/images/3/39/Esuna.png";
+                e.Description = $"{caster} performes an Esuna selfcast. It seems to have done something, but we're not sure what.";
+                return e;
+            }
+            else
+            {
+                e.ImageUrl = "https://ffxiv.consolegameswiki.com/mediawiki/images/3/39/Esuna.png";
+                e.Description = $"{caster} casts Esuna on {target}. It took 3 minutes, and had some visual effect. Noone knows if it actually worked.";
+                return e;
+            }
+        }
+    }
+}
