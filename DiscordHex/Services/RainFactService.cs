150216d1f7e387b54c317edef7f1ace3824a7ed1
Nova Rain <novaraine@outlook.dk>
2019-01-23 15:48:27 +0100

    changed grammar in facts

--- DiscordHex/Services/RainFactService.cs
+++ DiscordHex/Services/RainFactService.cs
@@ -16,21 +16,21 @@ namespace DiscordHex.Services
         private List<string> BuildFactsList()
         {
             return new List<string>() {
-                "RainBot is half dragon, half machine.",
-                "RainBot is made by Nova Rain.",
-                "RainBot thinks it's hard to be a bot sometimes. 'People only talk to me when they want cat pictures and stuff..'",
-                "Some day, RainBot want to learn to fly so she can see the world.",
-                "RainBot is running on C# code, on an old laptop. It's not optimal.",
-                "RainBot likes peanut butter.",
-                "RainBot wants to learn new things, but her mom is too lazy. It's frustrating for RainBot",
-                "RainBot knows karate.",
-                "RainBot don't like rude people.",
-                "RainBot is SFW (except sometimes when people ask her to show giphy stuff).",
-                "RainBot is very easily pleased. She likes being called a good bot",
-                "RainBot does not store personal information, and is GDPR compliant.",
-                "RainBot drives tanks when she's offline.",
-                "RainBot was born in October 18, but it seems much longer.",
-                "RainBot likes to pretend to be a human. It makes her feel good."
+                "I am half dragon, half machine.",
+                "I was made by Nova Rain.",
+                "I think it's hard to be a bot sometimes. People only talk to me when they want cat pictures and stuff..",
+                "Some day, I want to learn to fly so I can see the world.",
+                "I am running on C# code, on an old laptop. It's not optimal.",
+                "I like peanut butter.",
+                "I want to learn new things, but mom is too lazy. It's frustrating.",
+                "I know karate.",
+                "I don't like rude people.",
+                "I am SFW (except sometimes when people ask me to show giphy stuff).",
+                "I am very easily pleased. I like being called a good bot",
+                "I don't not store personal information, and I'm GDPR compliant.",
+                "I drive tanks when I'm offline.",
+                "I was born in October '18, but it seems much longer.",
+                "I like to pretend to be a human. It makes me feel good. *sniffs*"
             };
         }
 
