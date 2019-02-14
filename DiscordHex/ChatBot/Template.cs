e4e8c591e5e0f6b3f23a6cc982a5f6b04398d0dd
Tom <uninet@live.no>
2019-02-14 11:04:17 +0100

    tmp

---
+++ DiscordHex/ChatBot/Template.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DiscordHex.ChatBot
+{
+    public class Template
+    {
+        public string Pattern { get; set; }
+        public string Response { get; set; }
+        public TemplateExecutionEnum Execute { get; set; }
+    }
+}
---
+++ DiscordHex/ChatBot/TemplateExecutionEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordHex.ChatBot
+{
+    public enum TemplateExecutionEnum
+    {
+        GREETING
+    }
+}
